6392a33655f25ab26062674bd07ddaa433dff823
Maria Prylypchenko <mariaprylypchenko@gmail.com>
2020-05-12 23:56:18 +0300

    add basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.scss';
+import { Route, BrowserRouter, Switch } from "react-router-dom";
+import CreateProduct from "./components/CreateProduct";
+import Products from "./components/Products";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/CreateProduct" component={CreateProduct} />
+          <Route path="/" component={Products} />
+        </Switch>
+      </BrowserRouter>
   );
 }
 
