426a4f7b833a643863a05b049220b2b2ddf83816
Maria Prylypchenko <mariaprylypchenko@gmail.com>
2020-05-13 22:27:43 +0300

    add create product component

--- src/components/CreateProduct.tsx
+++ src/components/CreateProduct.tsx
@@ -1,7 +1,55 @@
 import React from 'react';
+import {Form, Button, Container} from "react-bootstrap";
+
 
 class CreateProduct extends React.Component {
+    state = {
+        name: "",
+        price: "",
+    };
 
+    handleInputChange = (event: { target: any; }) => {
+        const {target} = event;
+        const {value, name} = target;
+        this.setState({[name]: value});
+    };
+
+
+    handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <Container>
+                <form className="CreateProductForm" onSubmit={(event) => this.handleSubmit(event)}>
+                    <Form.Group>
+                        <Form.Label>Name</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter product name"
+                            onChange={this.handleInputChange}
+                            name="name"
+                            value={this.state.name}
+                        />
+                    </Form.Group>
+                    <Form.Group>
+                        <Form.Label>Price</Form.Label>
+                        <Form.Control
+                            type="number"
+                            placeholder="Enter product price"
+                            onChange={this.handleInputChange}
+                            name="price"
+                            value={this.state.price}
+                        />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </form>
+            </Container>
+        )
+    }
 }
 
 export default CreateProduct;
